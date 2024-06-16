58ef8335de4d8812eaeb4fed88d764affc3182f1
c4dr-me <142721314+c4dr-me@users.noreply.github.com>
2024-06-16 06:25:16 +0000

    copilot third commit

--- comment.js
+++ comment.js
@@ -1,41 +1,68 @@
-// Create web server
+//create web server
+var http = require('http');
 var express = require('express');
 var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var path = require('path');
-var url = require('url');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+//set view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/comment.html'));
+//use body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+//set public folder
+app.use(express.static('public'));
+
+//listen port
+server.listen(3000);
+
+//get home page
+app.get('/', function(req, res){
+  res.render('home');
 });
 
-app.post('/comment', function(req, res) {
-    var name = req.body.name;
-    var comment = req.body.comment;
-    var data = name + ': ' + comment + '\n';
-    fs.appendFile('comment.txt', data, function(err) {
-        if (err) {
-            res.send('Error');
-        } else {
-            res.send('Success');
-        }
-    });
+//get comment from client
+var comments = [];
+io.sockets.on('connection', function(socket){
+  socket.on('client-send-comment', function(data){
+    comments.push(data);
+    io.sockets.emit('server-send-comment', data);
+  });
 });
 
-app.get('/comment', function(req, res) {
-    fs.readFile('comment.txt', 'utf8', function(err, data) {
-        if (err) {
-            res.send('Error');
-        } else {
-            res.send(data);
-        }
-    });
+//get comment from server
+app.post('/comment', function(req, res){
+  var comment = req.body.comment;
+  var data = {
+    comment: comment
+  };
+  res.render('home', {data: data});
 });
 
-var server = app.listen(3000, function() {
-    console.log('Server is running at http://localhost:3000');
+//write comment to file
+app.post('/write', function(req, res){
+  var comment = req.body.comment;
+  fs.appendFile('comment.txt', comment + '\n', function(err){
+    if(err){
+      res.send('Error!');
+    }else{
+      res.send('Success!');
+    }
+  });
+});
+
+//read comment from file
+app.get('/read', function(req, res){
+  fs.readFile('comment.txt', 'utf8', function(err, data){
+    if(err){
+      res.send('Error!');
+    }else{
+      var arr = data.split('\n');
+      res.render('home', {comments: arr});
+    }
+  });
 });
